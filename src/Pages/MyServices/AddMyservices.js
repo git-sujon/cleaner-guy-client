abe790f4110e5c7d6e8e09a2c0a7badacfd308e2
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-09 18:38:59 +0600

    ADD services Functionality added

--- src/Pages/MyServices/AddMyservices.js
+++ src/Pages/MyServices/AddMyservices.js
@@ -1,39 +1,105 @@
+import { StarIcon } from "@heroicons/react/24/solid";
 import React, { useState } from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import { Link, useNavigate } from "react-router-dom";
 
 const AddMyservices = () => {
-  // const servicesDetails= { title, img_card, _id, description_one, price, rating }
+//   const { title, img_card, _id, description_one, price } = servicesDetails;
 
   const [rating, setRating] = useState(5);
-  console.log(rating);
+  const navigate= useNavigate()
+
+  const addServicesHandler = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title= form.title.value
+    const price = form.servicesPrice.value
+    const img_card =form.imgUrl.value
+    const description_one = form.description.value
+    // console.log(title, price, img_card, description_one,rating)
+
+    const services = {
+
+        title ,
+        rating,
+        img_card,
+        price,
+        description_one,
+      };
+
+    fetch(`http://localhost:5000/services`, {
+        method:"POST", 
+        headers: {
+            "content-type": "application/json"
+        },
+        body:JSON.stringify(services)
+    })
+    .then(res=> res.json())
+    .then(data => {
+        console.log(data);
+        navigate('/services')
+    })
+    .catch(err=> console.log(err))
+
+
+    
+
+  }
 
   return (
     <div>
       <section class="bg-gray-100">
+      <h1 className="text-5xl text-center pt-5 font-bold text-emerald-700">ADD A Services</h1>
         <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+           
           <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-
             {/* Live ShowCase  */}
 
-            <div class="lg:col-span-2 lg:py-12">
-              <p class="max-w-xl text-lg">
-                At the same time, the fact that we are wholly owned and totally
-                independent from manufacturer and other group control gives you
-                confidence that we will only recommend what is right for you.
-              </p>
+            <div className="w-full max-w-sm  rounded-lg shadow-md lg:col-span-2 bg-green-900">
+              <PhotoProvider>
+                <PhotoView src="https://www.hourmaid.com/wp-content/uploads/2017/12/cleaning-services-1024x682.jpeg">
+                  <img
+                    className=" rounded-t-lg h-[350px]"
+                    src="https://www.hourmaid.com/wp-content/uploads/2017/12/cleaning-services-1024x682.jpeg"
+                    alt="Services"
+                  />
+                </PhotoView>
+              </PhotoProvider>
 
-              <div class="mt-8">
-                <a href="" class="text-2xl font-bold text-pink-600">
-                  0151 475 4450
-                </a>
+              <div className="px-5 pb-5">
+                <Link to="#">
+                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    Home Cleaning
+                  </h5>
+                </Link>
+                <div className="flex items-center mt-2.5 mb-5">
+                  <StarIcon className="h-5 w-5 text-yellow-400" />
+                  <StarIcon className="h-5 w-5 text-yellow-400" />
+                  <StarIcon className="h-5 w-5 text-yellow-400" />
+                  <StarIcon className="h-5 w-5 text-yellow-400" />
+                  <StarIcon className="h-5 w-5 text-yellow-400" />
 
-                <address class="mt-2 not-italic">
-                  282 Kevin Brook, Imogeneborough, CA 58517
-                </address>
+                  <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                    {rating}
+                  </span>
+                </div>
+                {/* <p className="text-white ">{description_one.slice(0, 75)}...</p> */}
+                <div className="flex justify-between items-center">
+                  <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                    $249.99
+                  </span>
+                  <Link
+                    to={`/services/ndnadaldladkal`}
+                    className="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    View Details
+                  </Link>
+                </div>
               </div>
             </div>
 
             <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-              <form action="" class="space-y-4">
+              <form onSubmit={addServicesHandler} class="space-y-4">
                 {/* title  */}
 
                 <div>
@@ -57,31 +123,30 @@ const AddMyservices = () => {
                   <input
                     class="w-full rounded-lg border border-emerald-300 p-3 text-sm"
                     placeholder="Add a image url"
-                    type="email"
+                    type="text"
                     name="imgUrl"
                     id="email"
                   />
                 </div>
 
                 <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                    
-                 <div className="flex items-center ">
-                 <p className="mr-3 text-gray-400">Add Services Rating</p>
-                 <select
-                    className="w-1/5 bg-emerald-300  font-bold"
-                    onChange={(event) =>
-                      setRating(parseInt(event.target.value))
-                    }
-                  >
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option selected value="5">
-                      5
-                    </option>
-                  </select>
-                 </div>
+                  <div className="flex items-center ">
+                    <p className="mr-3 text-gray-400">Add Services Rating</p>
+                    <select
+                      className="w-1/5 bg-emerald-300  font-bold"
+                      onChange={(event) =>
+                        setRating(event.target.value)
+                      }
+                    >
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option selected value="5">
+                        5
+                      </option>
+                    </select>
+                  </div>
 
                   <div>
                     <label class="sr-only" for="phone">
@@ -113,7 +178,7 @@ const AddMyservices = () => {
                 <div class="mt-4">
                   <button
                     type="submit"
-                    class="inline-flex w-full items-center justify-center rounded-lg bg-emerald-600 px-5 py-3 text-white sm:w-auto"
+                    class="inline-flex w-full items-center justify-center rounded-lg hover:bg-emerald-800 bg-emerald-600 px-5 py-3 text-white sm:w-auto"
                   >
                     <span class="font-medium"> Add Services </span>
 
@@ -144,13 +209,4 @@ const AddMyservices = () => {
 
 export default AddMyservices;
 
-const x = {
-  _id: "6369dce37e39fc7a84ed2aab",
-  title: "House Cleaning",
-  rating: "4.5",
-  img_card:
-    "https://plus.unsplash.com/premium_photo-1661777227443-77e227eb8d35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
-  price: "250.99",
-  description_one:
-    "We use our unique Detail-Clean Rotation System to ensure that our customers’ homes always meet our high standard of cleanliness. This system was developed over 25+ years of professional house cleaning experience. We use this system to ensure that our clients are always satisfied and can expect a consistently clean and beautiful space.",
-};
+
