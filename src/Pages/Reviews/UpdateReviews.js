74d0c3d77a05e338024d6610d2f2e049c051533e
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-10 10:25:13 +0600

    Dyanamic Update Route created

--- src/Pages/Reviews/UpdateReviews.js
+++ src/Pages/Reviews/UpdateReviews.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const UpdateReviews = () => {
+    const review = useLoaderData()
+    console.log(review)
     return (
         <div>
             upppppppppppppppppppda
