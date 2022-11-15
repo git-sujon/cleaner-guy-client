2665215119277c9f9e655dc0d062df909be28d9f
Mr-Robot-101 <git.sujon@gmail.com>
2022-11-15 19:01:32 +0600

    Spinner Changed

--- src/Pages/Others/Sppiner.js
+++ src/Pages/Others/Sppiner.js
@@ -4,7 +4,10 @@ const Sppiner = () => {
   return (
     <div>
       <div className="flex items-center justify-center min-h-screen">
-      
+        <div
+          style={{ borderTopColor: "transparent" }}
+          className="w-20 h-20 border-4 border-green-900 rounded-full animate-spin"
+        ></div>
       </div>
     </div>
   );
